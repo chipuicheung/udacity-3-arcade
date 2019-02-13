fdf44fc4d4620a95699384b13790c272f9d0ca90
ChiPui <chipuicheung@hotmail.com>
2019-02-13 16:12:56 +0100

    Add player
    Player can move but not off the board

--- js/app.js
+++ js/app.js
@@ -1,27 +1,63 @@
 // Enemies our player must avoid
-var Enemy = function() {
-    // Variables applied to each of our instances go here,
-    // we've provided one for you to get started
-
-    // The image/sprite for our enemies, this uses
-    // a helper we've provided to easily load images
+class Enemy {
+  constructor() {    
+    this.x = 0;
+    this.y = 0;
     this.sprite = 'images/enemy-bug.png';
-};
-
+  }
 // Update the enemy's position, required method for game
 // Parameter: dt, a time delta between ticks
-Enemy.prototype.update = function(dt) {
-    // You should multiply any movement by the dt parameter
-    // which will ensure the game runs at the same speed for
-    // all computers.
-};
-
+//   update() {
+// // You should multiply any movement by the dt parameter
+//     // which will ensure the game runs at the same speed for
+//     // all computers.
+//   }
 // Draw the enemy on the screen, required method for game
-Enemy.prototype.render = function() {
+  render() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-};
+  }
+}
 
 // Now write your own player class
+class Player {
+  constructor() {
+    this.x = 200;
+    this.y = 404;
+    this.startX = this.x;
+    this.startY = this.y;
+    this.horz = 101; // x
+    this.vert = 83; // y  
+    this.sprite = 'images/char-princess-girl.png';
+  }
+
+  render() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+  }
+
+  handleInput(allowedKeys) {
+    let pressKey = allowedKeys;
+    if (this.x > 0) {
+      if (pressKey === 'left') {
+        this.x -= this.horz;
+      }
+    }
+    if (this.x < this.horz * 3) {
+      if (pressKey === 'right') {
+        this.x += this.horz;
+      }
+    }
+    if (this.y > 0){
+      if (pressKey === 'up') {
+        this.y -= this.vert;
+      }
+    } 
+    if (this.y < this.vert * 4) { 
+      if (pressKey === 'down') {
+        this.y += this.vert;
+      }
+    }
+  }
+}
 // This class requires an update(), render() and
 // a handleInput() method.
 
@@ -29,6 +65,7 @@ Enemy.prototype.render = function() {
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
+const player = new Player();
 
 
 
